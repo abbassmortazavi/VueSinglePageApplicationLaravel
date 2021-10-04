784faf9d8a1dfabf7b858ba4a900766acdfe8e4f
Abbass Mortazavi <abbassmortazavi@gmail.com>
2021-10-04 11:24:32 +0330

    create Login and Register page

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,7 +21,37 @@ window.toast = Swal.mixin({
         toast.addEventListener('mouseenter', Swal.stopTimer)
         toast.addEventListener('mouseleave', Swal.resumeTimer)
     }*/
-})
+});
+
+function loggedIn(){
+    return false;
+}
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+      // this route requires auth, check if logged in
+      // if not, redirect to login page.
+      if (!loggedIn()) {
+        next({
+          path: '/login',
+          query: { redirect: to.fullPath }
+        })
+      } else {
+        next()
+      }
+    } else if(to.matched.some(record => record.meta.guest)) {
+        if (loggedIn()) {
+            next({
+              path: '/',
+              query: { redirect: to.fullPath }
+            })
+          } else {
+            next()
+          }
+    }else{
+        next() // make sure to always call next()!
+    }
+  });
 
 const app = new Vue({
     el: '#app',
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -4,27 +4,54 @@ import VueRouter from 'vue-router';
 import Welcome from './components/Welcome';
 import Create from './components/Create';
 import Edit from './components/Edit';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: '/',
         component : Welcome,
-        name: 'Welcome'
+        name: 'Welcome',
+        meta:{
+            requiresAuth: true
+        } 
     },
     {
         path: '/create',
         component : Create,
-        name: 'Create'
+        name: 'Create',
+        meta:{
+            requiresAuth: true
+        }
     },
     {
         path: '/edit/:id',
         component : Edit,
-        name: 'Edit'
+        name: 'Edit',
+        meta:{
+            requiresAuth: true
+        }
     },
     {
         path: '*',
         component : Welcome
+    },
+    {
+        path: '/login',
+        component: Login,
+        name: 'Login',
+        meta:{
+            guest: true
+        }
+    },
+    {
+        path: '/register',
+        component: Register,
+        name: 'Register',
+        meta:{
+            guest: true
+        }
     }
 ]
 
